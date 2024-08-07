5cab80a56d953e7a23ab6df86650392c7c213234
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-08-07 11:15:49 +0200

    click event

--- js/build.js
+++ js/build.js
@@ -87,7 +87,9 @@ Fliplet.Widget.instance({
         }
 
         function setActionClickEvent() {
-          $(document).off('click', '.social-actions').on('click', '.social-actions', function() {
+          $(document).off('click', '.social-actions').on('click', '.social-actions', function(e) {
+            e.stopPropagation();
+
             const $thisClick = $(this);
             const entryDataSource = $thisClick.data('original-datasource-id');
             const globalDataSourceId = $thisClick.data('global-datasource-id');
