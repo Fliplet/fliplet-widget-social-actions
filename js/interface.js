1513610148b1aba3c0eafa093738a4d0dbe7c54e
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-08-12 07:59:40 +0200

    reverted

--- js/interface.js
+++ js/interface.js
@@ -86,10 +86,7 @@ Fliplet.DataSources.get({
           type: 'radio',
           label: 'Select type for social feature',
           options: ['Bookmark', 'Like'],
-          default: 'Bookmark',
-          change: function(val) {
-            debugger;
-          }
+          default: 'Bookmark'
         },
         {
           name: 'socialDataSourceId',
